1e290d818d2d3808133aac863122890caf7691e7
Skellowyin <theo.benavente@elev.ga.lbs.se>
2021-10-25 14:10:40 +0200

    Music works

--- Assets/Sounds/Scripts/AudioManager.cs
+++ Assets/Sounds/Scripts/AudioManager.cs
@@ -11,10 +11,12 @@ public class AudioManager : MonoBehaviour
     {
         foreach(Sound s in sounds)
         {
-            gameObject.AddComponent<AudioSource>();
+            s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
+
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
